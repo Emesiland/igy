c999135be64657551c5595a34022be975e887b9c
Emesiland <emesiland@mail.ru>
2025-11-05 14:05:00 +0400

    Доработал функцию, проверил работу

--- IGY.py
+++ IGY.py
@@ -1,42 +1,5 @@
 #1
-'''
-class FlatIterator:
-
-    def __init__(self, list_of_list):
-        ...
-
-    def __iter__(self):
-        ...
-        return self
-
-    def __next__(self):
-        ...
-        return item
-
-
-def test_1():
-
-    list_of_lists_1 = [
-        ['a', 'b', 'c'],
-        ['d', 'e', 'f', 'h', False],
-        [1, 2, None]
-    ]
-
-    for flat_iterator_item, check_item in zip(
-            FlatIterator(list_of_lists_1),
-            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
-    ):
-
-        assert flat_iterator_item == check_item
-
-    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
-
-
-if __name__ == '__main__':
-    test_1()
-'''
-
-class FlatIterator:
+'''class FlatIterator:
 
     def __init__(self, list_of_list):
         self.list_of_list = list_of_list
@@ -88,9 +51,45 @@ for item in FlatIterator(list_of_lists_1):
     print(item)
     
 print("\nСписок из итератора:")
-print(list(FlatIterator(list_of_lists_1)))
+print(list(FlatIterator(list_of_lists_1)))'''
 
 #2
-'''
+import types
 
-'''
+def flat_generator(list_of_lists):
+    for inner_list in list_of_lists:
+        for item in inner_list:
+            yield item
+
+def test_2():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+if __name__ == '__main__':
+    test_2()
+
+    print("Тест пройден")
+
+    list_of_lists_2 = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    
+    print("\nПроверка:")
+    flat_list = list(flat_generator(list_of_lists_2))
+    print(f"Исходный список: {list_of_lists_2}")
+    print(f"Плоский список: {flat_list}")
